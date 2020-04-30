55145619378e06b3a98bcda0701e803b56827485
vtr <vrigolon@gmail.com>
2020-04-30 23:09:31 +0100

    create/post route

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,48 @@
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 module.exports = {
   create(req, res) {
-    return res.render("products/create.njk")
-  }
+
+    Category.all()
+    .then(function(results) {
+
+      const categories = results.rows
+      return res.render("products/create.njk", { categories })
+
+    }).catch(function(err) {
+
+      throw new Error(err)
+    })
+
+  },
+  async post(req, res) {
+
+    const keys = Object.keys(req.body)
+    
+    // for(key of keys) {
+    //   if(req.body[key] == "") return res.send('Please, fill all the fields')
+        
+    // }
+    
+    let results = await Product.create(req.body)
+    const productId = results.rows[0].id
+
+    results = await Category.all()
+    const categories = result.rows
+
+    return res.redirect(`products/${productID}`)
+  },
+  async edit(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if(!product) return res.send("Product not found!")
+
+    results = await Category.all()
+    const categories = result.rows
+
+    return res.render("products/edit.njk", { product, categories })
+  } 
 }
--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,16 @@ routes.get('/', function(req, res) {
   return res.render("layout.njk")
 })
 
+
+
 routes.get('/products/create', ProductController.create)
 
+routes.get('/products/:id/edit', ProductController.edit)
+routes.post('/products', ProductController.post)
 
+
+
+//alias
 routes.get('/ads/create', function(req, res) {
   return res.redirect("/products/create")
 })
