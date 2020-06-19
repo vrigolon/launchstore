b892258a7071af19232e1b9da01900dcec29fd9a
vtr <vrigolon@gmail.com>
2020-06-19 19:48:40 +0100

    add 'upload photos'

--- public/scripts.js
+++ public/scripts.js
@@ -18,3 +18,37 @@ const Mask = {
 
   }
 }
+
+const PhotosUpload = {
+  uploadLimit: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+    const { uploadLimit } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return
+    }
+
+    Array.from(fileList).forEach(file => {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = String(reader.result)
+
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = () => alert('remover photo')
+
+        div.appendChild(image)
+
+        document.querySelector('#photos-preview').appendChild(div)
+      }
+
+      reader.readAsDataURL(file)
+    })
+  }
+}
