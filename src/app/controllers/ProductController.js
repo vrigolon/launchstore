2b0dec93ae46e970ccb8aa05a6865f91f117cf3a
vtr <vrigolon@gmail.com>
2020-06-19 12:34:13 +0100

    add delete and update functions on edit page

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -22,10 +22,11 @@ module.exports = {
 
     const keys = Object.keys(req.body)
     
-    // for(key of keys) {
-    //   if(req.body[key] == "") return res.send('Please, fill all the fields')
-        
-    // }
+    for(key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Please, fill all the fields')
+      }
+    }
     
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
@@ -49,5 +50,29 @@ module.exports = {
     const categories = results.rows
 
     return res.render("products/edit.njk", { product, categories })
-  } 
+  },
+  async put(req, res) {
+    const keys = Object.keys(req.body)
+    
+    for(key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Please, fill all the fields')
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g, "")
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id)
+
+      req.body.old_price = oldProduct.rows[0].price
+    }
+
+    await Product.update(req.body)
+
+    return res.redirect(`/products/${req.body.id}/edit`)
+  },
+  async delete(req, res) {
+    await Product.delete(req.body.id)
+    return res.redirect('/products/create')
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,8 @@ routes.get('/products/create', ProductController.create)
 
 routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
 
 
 
