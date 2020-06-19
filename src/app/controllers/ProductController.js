59833de7ff69a76caa673bf128a7c3cc65706cde
vtr <vrigolon@gmail.com>
2020-06-19 10:12:52 +0100

    add edit page

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require('../../lib/utils')
+
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
@@ -40,8 +42,11 @@ module.exports = {
 
     if(!product) return res.send("Product not found!")
 
+    product.old_price = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
     results = await Category.all()
-    const categories = result.rows
+    const categories = results.rows
 
     return res.render("products/edit.njk", { product, categories })
   } 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -14,5 +14,11 @@ module.exports = {
     format: `${day}/${month}/${year}`
 
     }
+  },
+  formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price/100)
   }
 }
