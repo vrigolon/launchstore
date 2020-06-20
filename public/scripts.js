eddebeb2bab0fbe62c322ecec400b502cba8c359
vtr <vrigolon@gmail.com>
2020-06-20 13:23:40 +0100

    add front-end photos upload managing

--- public/scripts.js
+++ public/scripts.js
@@ -20,16 +20,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'),
   uploadLimit: 6,
   handleFileInput(event) {
     const { files: fileList } = event.target
-    const { uploadLimit } = PhotosUpload
-
-    if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} fotos`)
-      event.preventDefault()
-      return
-    }
+    
+    if (PhotosUpload.hasLimit(event)) return
 
     Array.from(fileList).forEach(file => {
       const reader = new FileReader()
@@ -38,17 +34,48 @@ const PhotosUpload = {
         const image = new Image()
         image.src = String(reader.result)
 
-        const div = document.createElement('div')
-        div.classList.add('photo')
+        const div = PhotosUpload.getContainer(image)
 
-        div.onclick = () => alert('remover photo')
-
-        div.appendChild(image)
-
-        document.querySelector('#photos-preview').appendChild(div)
+        PhotosUpload.preview.appendChild(div)
       }
 
       reader.readAsDataURL(file)
     })
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload
+    const { files: fileList } = event.target
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return true
+    }
+    return false
+
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+    div.classList.add('photo')
+
+    div.onclick = PhotosUpload.removePhoto
+
+    div.appendChild(image)
+
+    div.appendChild(PhotosUpload.getRemoveButton())
+
+    return div
+  },
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = "close"
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(photoDiv)
+    photoDiv.remove()
   }
 }
